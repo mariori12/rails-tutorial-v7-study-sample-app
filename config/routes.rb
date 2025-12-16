8560104416450573095d2846ccfd3d493d5cd6b9
mariori12 <syutorum002@gmail.com>
2025-12-16 22:18:11 +0900

    Update: HomeページをルートURLに設定する
    HomeページをルートURLに設定する。
    rootルーティングの記述位置を調整する。

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  # Defines the root path route ("/")
+  # root "articles#index"
+  root 'static_pages#home'
+
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
   get 'static_pages/contact'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  root 'application#hello'
 end
